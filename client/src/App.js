0d78f16a1ea5bb76d28d8055e9ed5326aad8347e
Spencer LaPray <sblapray@yahoo.com>
2019-06-01 00:06:56 -0600

    pushing latest

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import AddExperience from "./components/profile-form/AddExperience";
 import AddEducation from "./components/profile-form/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
@@ -62,6 +64,8 @@ const App = () => {
                 path="/add-education"
                 component={AddEducation}
               />
+              <PrivateRoute exact path="/posts" component={Posts} />
+              <PrivateRoute exact path="/posts/:id" component={Post} />
             </Switch>
             <Alert />
           </section>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,6 +11,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to="/profiles">Developers</Link>
       </li>
       <li>
+        <Link to="/posts">Posts</Link>
+      </li>
+      <li>
         <Link to="/dashboard">
           <i className="fas fa-user" /> <span>Dashboard</span>
         </Link>
@@ -42,7 +45,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code" /> DevConnector
+          <i className="fas fa-code" /> The Dev Network
         </Link>
       </h1>
       {!loading && (
